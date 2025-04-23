acdd11a198d88f4534393b1ed8dde46728f4df40
Liudmyla Rybak <l7327833@gmail.com>
2025-04-23 16:11:07 +0200

    Add files via upload

---
+++ BusinessContact.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "AbstractContact.h"
+#include <string>
+#include <iostream>
+class BusinessContact : public AbstractContact
+{
+private:
+	std::string workPhone;
+	std::string workEmail;
+
+public:
+	BusinessContact(){}
+	BusinessContact(std::string fn, std::string sr, std::string wph, std::string we)
+		: AbstractContact(fn, sr), workPhone(wph), workEmail(we){}
+
+	void display() override {
+		std::cout << "Firstname: " << getFirstname() << " "
+			<< "Surname: " << getSurname() << " "
+			<< "workPhone: " << workPhone << " "
+			<< "workEmail: " << workEmail << "\n";
+	}
+
+	void status() override {
+		std::cout << "Business contact";
+	}
+
+
+
+	void setWorkPhone(const std::string& wph) {  // the parameter will not changed in method
+		this->workPhone = wph;
+	}
+
+	void setWorkEmail(const std::string& we) {
+		this->workEmail = we;
+	}
+
+	std::string getWorkPhone() const { // this method will not change anything
+		return workPhone;
+	}
+
+	std::string getWorkEmail() const {
+		return workEmail;
+	}
+
+};
+
+
+
+
+// diffrance const std::string getWorkPhone()  - Возвращается строка, но это бесполезно. The returned value is a const std::string. “Возвращается const std::string, которую нельзя менять… но так как это копия — ты всё равно можешь её изменить.”
+
+// getWorkPhone() co2nst - Метод ничего не меняет. “Этот метод ничего не меняет в объекте.”Его можно вызывать даже у const объектов.
+
+
---
+++ ContactsManager.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <memory>
+#include <vector>
+#include "AbstractContact.h"
+class ContactsManager
+{
+private:
+	std::vector<std::unique_ptr<AbstractContact>> contacts;
+
+public:
+
+	ContactsManager();
+
+	void addContact(std::unique_ptr<AbstractContact> contact);
+
+	void searchByFName(const std::string& fname);
+
+	void deleteContact(std::unique_ptr<AbstractContact> contact);
+
+	void editContact();
+
+	void displayAll();
+
+};
+
+
+
+//contacts = [
+//	[unique_ptr]→ 🧍 AbstractContact(PersonalContact),
+//	[unique_ptr] → 🧍 AbstractContact(BusinessContact),
+//	[unique_ptr] → 🧍 AbstractContact(PersonalContact),
+//]
+
+
+
+// Goals are using smart pointers 
+//			*There are*
+//	 1. std::unique_ptr<someType>	&&	std::make_unique<sometype>()
+//	 2. std::shared_ptr<someType>	&&	std::make_shared<T>()			&& use_count(), reset()
+//	 3. std::weak_ptr<someType>
+
+// syntax 
+// std::unique_ptr<T>
+// where T = AbstractContact
+// a smart pointer that owns an object of type AbstractContact, and automatically deletes it when destroyed
+// unique_ptr is a class template, and you must specify what type it owns
