1e211be668e1ce7718d8141b7b275ae8b37f990f
Liudmyla Rybak <l7327833@gmail.com>
2025-05-21 21:47:11 +0200

    Update Knight's Tour.cpp

--- DataStructure&Algorithms/Knight's Tour.cpp
+++ DataStructure&Algorithms/Knight's Tour.cpp
@@ -9,9 +9,9 @@ using Matrix = Acces_to::vector<Acces_to::vector<Integer>>;
 struct Board {
 public: 
 	Matrix board = {
-	{1,	 2,	 3,	 4,  5},
-	{6,	 7,	 8,	 9,	 10},
-	{11, 12, 0,	 13, 14},
+	{1,  2,	 3,  4,  5},
+	{6,  7, 8,  9,	 10},
+	{11, 12, 0,  13, 14},
 	{15, 16, 17, 18, 19},
 	{20, 21, 22, 23, 24}
 	};
