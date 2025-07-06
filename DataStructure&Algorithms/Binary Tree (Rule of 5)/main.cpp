f6c8d669ae209ef52bef202a735288661c02e73f
Liudmyla Rybak <l7327833@gmail.com>
2025-07-06 18:51:56 +0200

    Update main.cpp

--- DataStructure&Algorithms/Binary Tree (Rule of 5)/main.cpp
+++ DataStructure&Algorithms/Binary Tree (Rule of 5)/main.cpp
@@ -1,9 +1,10 @@
+#pragma once
 #include <iostream>
-#include <Tree.h>
+#include "BTree.h"
 
 int main() {
 	std::cout << "Binary Tree" << "\n";
-	Tree<int> my_tree;
+	BTree<int> my_tree;
 	auto data = 7;
 	my_tree.insert(3);
 	my_tree.insert(data);
