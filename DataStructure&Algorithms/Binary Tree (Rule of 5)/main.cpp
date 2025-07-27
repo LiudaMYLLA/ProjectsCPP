fcae8b11b5c7ee1772e06dedfa1ba6664ffc68ff
Liudmyla Rybak <l7327833@gmail.com>
2025-07-27 21:19:11 +0200

    Update main.cpp

--- DataStructure&Algorithms/Binary Tree (Rule of 5)/main.cpp
+++ DataStructure&Algorithms/Binary Tree (Rule of 5)/main.cpp
@@ -1,11 +1,26 @@
 #pragma once
 #include <iostream>
-#include "BTree.h"
+#include "BTree.hpp"
+#include "Iterator.hpp"
 
 int main() {
 	std::cout << "Binary Tree" << "\n";
 	BTree<int> my_tree;
-	auto data = 7;
+	const int data = 7;
 	my_tree.insert(3);
 	my_tree.insert(data);
+	my_tree.insert(100);
+
+	Iterator<int> it = my_tree.begin();
+	Iterator<int> it2 = my_tree.begin();
+
+	Iterator<int> end = my_tree.end();
+
+	while (it != end) {
+		std::cout << *it << " ";
+		++it;
+
+		auto temp = it2++; // temp saves old from post-increment
+		std::cout << *temp << " " << *it2 << " ";
+	}
 }
